1ef76425134f1651852e0d07c5819619456829e8
ualiurrahat <ualiurrahat25@gmail.com>
2023-09-11 02:12:19 +0600

    created header,menu,footer components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -49,11 +49,26 @@ const pizzaData = [
 
 function App() {
     return (<div>
-        <h1>Hello React!</h1>
-        <Pizza />
-        <Pizza />
-        <Pizza />
+        <Header />
+        <Menu />
+        <Footer />
     </div>);
+}
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>;
+}
+function Menu() {
+    return (<div>
+        <h2>Our Menu</h2>
+        <Pizza />
+        <Pizza />
+        <Pizza />
+        <Pizza />
+    </div>)
+}
+function Footer() {
+    // return React.createElement("footer", null, "We're currently open!");
+    return <footer>{new Date().toLocaleTimeString()}.We're currently open.</footer>
 }
 function Pizza() {
     return (<div>
