658e261cc77ab36dd6b13be22e648a5a1233d9f1
ualiurrahat <ualiurrahat25@gmail.com>
2023-09-11 01:32:34 +0600

    added necessary files and created,reused pizza component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,9 +1,68 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'; // react before 18: import ReactDOM from 'react-dom'
 
+// pizza data
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
 
 function App() {
-    return <h1>Hello React!</h1>;
+    return (<div>
+        <h1>Hello React!</h1>
+        <Pizza />
+        <Pizza />
+        <Pizza />
+    </div>);
+}
+function Pizza() {
+    return (<div>
+        <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+
+    </div>
+    );
 }
 
 // react v18
@@ -13,7 +72,7 @@ root.render(<React.StrictMode>
 </React.StrictMode>);
 
 // react before 18
-// React.render(<App/>)
+// ReactDOM.render(<App/>,(document.getElementById("root"));
 // about StrictMode: renders our components twice
 // to check if there is any bug or any outdated components.
 // good practice to use StrictMode
