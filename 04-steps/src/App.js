fecd82351461d4077796e7efdba9e338f2d113c4
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-07 22:44:50 +0600

    updated comment in steps app

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,6 +8,16 @@ const messages = [
 
 
 export default function App() {
+  // Note: state should be delcared only in the scope of component
+  // not insid of any other statement or loop,expression.
+  // ex: if(true)
+  // {
+  // const [step,setStep] = useState(1); 
+  // }
+  // this is not allowed. Becuase it is inside App component.
+  // then inside if-else statement. It is not allowed.
+  //NOTE: State should be declared just inside the component's scope only.
+
   // declaring state to change step value
   const [step, setStep] = useState(1);
   // declaring another state for UI(cross button)
@@ -27,7 +37,7 @@ export default function App() {
 
   // BAD Practice
   // test.name = "murr";
-  // setTest({test.name = "murr"};)
+  // GOOD PRACTICE: setTest({test.name = "murr"};)
   return (
     // using react fragment as we are returning two things
     // X(cross button) and full steps div
@@ -37,9 +47,9 @@ export default function App() {
       <button className="close" onClick={() => setIsOpen((is) => !is)}>&times;</button>
       {isOpen && (<div div className="steps" >
         <div className="numbers">
-          <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-          <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-          <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+          <div className={step >= 1 ? 'active' : ''}>1</div>
+          <div className={step >= 2 ? 'active' : ''}>2</div>
+          <div className={step >= 3 ? 'active' : ''}>3</div>
         </div>
         <p className="message">Step {step} : {messages[step - 1]}</p>
         <div className="buttons">
