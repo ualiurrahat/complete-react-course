b143f46a711a82ef0d610848c923ac629f6afaca
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-12 00:29:25 +0600

    j

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -21,10 +21,18 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -34,7 +42,7 @@ function PackingList() {
       <ul >
         {
           initialItems.map((item) => (
-            <Item item={item} />
+            <Item item={item} key={item.id} />
           ))
         }
       </ul>
