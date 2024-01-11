df72b6daeea5ec3f7dc2fbc26f239c2d6e77c849
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-12 00:30:18 +0600

    built a form and handled submission

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -25,14 +25,14 @@ function Form() {
     e.preventDefault();
   }
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
+    < form className="add-form" onSubmit={handleSubmit} >
       <h3>What do you need for your 😍 trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
-    </form>
+    </ >
   );
 }
 
