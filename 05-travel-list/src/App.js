aa185f40036f76b25679aed5918536abeee4d6f8
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-15 14:55:57 +0600

    added controlled elements

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -21,18 +24,32 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
+    // if user input null string as description, add nothing in the items list
+    if (!description) return;
+    // adding new items from user given submit form
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
   return (
-    < form className="add-form" onSubmit={handleSubmit} >
+    <form form className="add-form" onSubmit={handleSubmit} >
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)
+        } />
       <button>Add</button>
-    </ >
+    </form>
   );
 }
 
