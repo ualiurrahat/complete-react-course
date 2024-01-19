f0826a5872450d5b5255b34c5ee6ae1f0a301813
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-19 20:13:16 +0600

    added delete operation functionality in each list of the item

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,25 +1,21 @@
 import { useState } from "react";
 
-
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true }
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
   function handleAddItems(item) {
-    setItems(items => [...items, item]);
+    setItems((items) => [...items, item]);
+  }
+  // function to delete items from list
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
   }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
-
   );
 }
 
@@ -30,7 +26,6 @@ function Logo() {
 function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -44,41 +39,47 @@ function Form({ onAddItems }) {
     setQuantity(1);
   }
   return (
-    <form form className="add-form" onSubmit={handleSubmit} >
+    <form form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (<option value={num} key={num}>{num}</option>))}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input
         type="text"
         placeholder="Item..."
         value={description}
-        onChange={(e) => setDescription(e.target.value)
-        } />
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
-      <ul >
-        {
-          items.map((item) => (
-            <Item item={item} key={item.id} />
-          ))
-        }
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
+        ))}
       </ul>
     </div>
   );
 }
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}</span>
-      <button>❌</button>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
