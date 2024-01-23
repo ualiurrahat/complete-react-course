50c8793cde8a67806c371b72e83794424d1048b3
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-24 00:25:54 +0600

    added a sorting feature of items by 3 different values

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -76,10 +76,28 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  // state to update sotring list by user given input
+  const [sortBy, setSortBy] = useState("input");
+  // to store items in sorted fashion
+  let sortedItems;
+  // sorting by input : user provided order
+  if (sortBy === "input") sortedItems = items;
+  // sorted by items description.
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  // sorting by packed status : packed items will go to end of the list
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -88,6 +106,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
