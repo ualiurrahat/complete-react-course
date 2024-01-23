a3bf2d0f29063d78903fa6df70bd48b971f09bdd
ualiurrahat <ualiurrahat25@gmail.com>
2024-01-23 23:54:21 +0600

    calculated stats as derived state

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -108,10 +108,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  // if there is no item
+  // show this message
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+  }
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X(X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have ${numItems} items on your list, and you already packed ${numPacked}
+        (${percentage}%)`}
+      </em>
     </footer>
   );
 }
